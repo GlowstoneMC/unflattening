37d319803bfd8f474b7fac7a92e3f438cc6f93c2
momothereal <momothereal.mc@gmail.com>
2018-08-06 04:21:44 -0400

    Property maps/enums

--- templates.py
+++ templates.py
@@ -18,11 +18,93 @@ JAVA_HEADER = """/*
  */
 """
 
+MATERIAL_PROPERTIES_CLASS = \
+"""static final ImmutableMap<Material, PropertyDefs> MATERIAL_PROPERTIES;
+
+static class PropertyDefs {
+    private final PropMap properties;
+
+    PropertyDefs(PropMap properties) {
+        this.properties = properties;
+    }
+    private int serialize(Map<String, Object> values) {
+        if (values == null) {
+            return 0;
+        }
+        int mask = 0;
+        for (int i = this.properties.keys.length - 1; i >= 0; i--) {
+            String key = this.properties.keys[i];
+            PropertyEnum propEnum = this.properties.enums[i];
+            mask |= propEnum.serialize(values.getOrDefault(key, propEnum.defaultValue()));
+        }
+        return mask;
+    }
+    private Map<String, Object> deserialize(int serial) {
+        int sizeAcc = 0;
+        ImmutableMap.Builder<String, Object> result = ImmutableMap.builder();
+        for (int i = this.properties.keys.length - 1; i >= 0; i--) {
+            String key = this.properties.keys[i];
+            PropertyEnum propEnum = this.properties.enums[i];
+            int enumSize = (int) Math.ceil(Math.sqrt(propEnum.values.length));
+            int x = ((1 << enumSize) - 1) & (serial >> ((sizeAcc + 1) - 1));
+            sizeAcc += enumSize;
+            result.put(key, propEnum.deserialize(x));
+        }
+        return result.build();
+    }
+}
+
+
+static class PropertyEnum {
+    private final String[] values;
+    public PropertyEnum(String... values) {
+        this.values = values;
+    }
+    public int serialize(Object value) {
+        return ArrayUtils.indexOf(values, value);
+    }
+    public Object deserialize(int serial) {
+        return values[serial];
+    }
+    public Object defaultValue() {
+        return values[0];
+    }
+}
+static class PropMap {
+    private final String[] keys;
+    private final PropertyEnum[] enums;
+
+    public PropMap(String[] keys, PropertyEnum[] enums) {
+        this.keys = keys;
+        this.enums = enums;
+    }
+}
+private static PropMap propMapOf(Object... keysAndVals) {
+    Iterator<Object> iter = Iterators.forArray(keysAndVals);
+    List<String> keys = new ArrayList<>();
+    List<PropertyEnum> enums = new ArrayList<>();
+    while (iter.hasNext()) {
+        keys.add((String) iter.next());
+        enums.add((PropertyEnum) iter.next());
+    }
+    return new PropMap(keys.toArray(new String[0]), enums.toArray(new PropertyEnum[0]));
+}
+"""
+
+MATERIAL_PROPERTIES_STATIC = \
+"""ImmutableMap.Builder<Material, PropertyDefs> materialPropertiesBuilder = ImmutableMap.builder();
+materialPropertiesBuilder
+{put_statements}
+;
+MATERIAL_PROPERTIES = materialPropertiesBuilder.build();
+"""
+
+
 DIRECTIONAL_POSSIBLE_FACES_CLASS = "static final ImmutableMap<Material, List<BlockFace>> " \
                                    "DIRECTIONAL_POSSIBLE_FACES;\n"
 
-DIRECTIONAL_POSSIBLE_FACES_STATIC = """ImmutableMap.Builder<Material, List<BlockFace>> possibleFacesBuilder """ \
-"""= ImmutableMap.builder();
+DIRECTIONAL_POSSIBLE_FACES_STATIC = \
+"""ImmutableMap.Builder<Material, List<BlockFace>> possibleFacesBuilder = ImmutableMap.builder();
 possibleFacesBuilder
 {put_statements}
 ;
--- unflattening.py
+++ unflattening.py
@@ -22,8 +22,13 @@ def make_java_class(package="net.glowstone.block.flattening.generated", class_na
     static_content = ""
     imports = (
         "com.google.common.collect.ImmutableMap",
+        "com.google.common.collect.Iterators",
         "java.util.Arrays",
+        "java.util.ArrayList",
+        "java.util.Iterator",
         "java.util.List",
+        "java.util.Map",
+        "org.apache.commons.lang.ArrayUtils",
         "org.bukkit.Material",
         "org.bukkit.block.BlockFace",
     )
@@ -31,25 +36,46 @@ def make_java_class(package="net.glowstone.block.flattening.generated", class_na
     # parse the data
     data = load_data()
     block_faces = OrderedDict()
+    properties = OrderedDict()
     for block_type in data.keys():
         block = data[block_type]
+        mat = to_bukkit_material_enum(block_type)
         if "properties" in block:
             props = block["properties"]
+            properties[mat] = props
             if "facing" in props:
-                block_faces[to_bukkit_material_enum(block_type)] = props["facing"]
+                block_faces[mat] = props["facing"]
+        else:
+            properties[mat] = {}
 
     # write the java file
+    def _props_map(props):
+        return ", ".join((
+            "\"{key}\", new PropertyEnum({values})".format(key=key,
+                                                           values=", ".join(("\"" + value + "\"" for value in values)))
+            for key, values in props.items()
+        ))
+
+    class_content += templates.MATERIAL_PROPERTIES_CLASS
+    static_content += templates.MATERIAL_PROPERTIES_STATIC.format(
+        put_statements=_indent(4, "\n".join((
+            ".put(Material.{mat}, "
+            "new PropertyDefs(propMapOf({props})))".format(mat=mat,
+                                                                 props=_props_map(props))
+            for mat, props in properties.items()
+        )))
+    )
+
     class_content += templates.DIRECTIONAL_POSSIBLE_FACES_CLASS
-    static_content += templates.DIRECTIONAL_POSSIBLE_FACES_STATIC\
-        .format(
-            put_statements=_indent(4, "\n".join((
-                ".put(Material.{mat}, Arrays.asList({faces}))".format(mat=mat,
-                                                                      faces=", ".join(
-                                                                          ("BlockFace." + face.upper() for face in
-                                                                           faces)))
-                for mat, faces in block_faces.items()
-            )))
-        )
+    static_content += templates.DIRECTIONAL_POSSIBLE_FACES_STATIC.format(
+        put_statements=_indent(4, "\n".join((
+            ".put(Material.{mat}, Arrays.asList({faces}))".format(mat=mat,
+                                                                  faces=", ".join(
+                                                                      ("BlockFace." + face.upper() for face in
+                                                                       faces)))
+            for mat, faces in block_faces.items()
+        )))
+    )
 
     # output
     return templates.JAVA_FILE.format(
